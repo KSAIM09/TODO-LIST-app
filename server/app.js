22e9f28498bb75d3497148f5d5282fccadc739eb
Saim Khan <93315183+KSAIM09@users.noreply.github.com>
2024-09-01 13:32:54 +0000

    update db

--- server/app.js
+++ server/app.js
@@ -7,7 +7,11 @@ const app = express();
 const Connection = require('./database/db')
 const Router = require('./routes/route')
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://todo-list-app-beryl-rho.vercel.app', // Allow only your Vercel domain
+    methods: 'GET,POST,PUT,DELETE', // Specify allowed methods
+    credentials: true // If you need to send cookies or authorization headers
+}));
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }))
 
--- server/database/db.js
+++ server/database/db.js
@@ -1,26 +1,24 @@
 const mongoose = require('mongoose');
 
-
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
 const Connection = () => {
-
     const MONGODB_URI = `mongodb://${USERNAME}:${PASSWORD}@todolist-shard-00-00.r5i3g.mongodb.net:27017,todolist-shard-00-01.r5i3g.mongodb.net:27017,todolist-shard-00-02.r5i3g.mongodb.net:27017/?ssl=true&replicaSet=atlas-4enqdj-shard-0&authSource=admin&retryWrites=true&w=majority&appName=TodoList`;
 
     mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
     mongoose.connection.on('connected', () => {
-        console.log('Database connected Successfully');
-    })
+        console.log('Database connected successfully');
+    });
 
     mongoose.connection.on('disconnected', () => {
         console.log('Database disconnected');
-    })
+    });
 
-    mongoose.connection.on('error', () => {
-        console.log('Error while connecting with the database ', error.message);
-    })
-}
+    mongoose.connection.on('error', (error) => {  // Pass error object here
+        console.log('Error while connecting with the database:', error.message);
+    });
+};
 
 module.exports = Connection;
