980f595646066591b8f30b2129f08f9dd3939c1a
Saim Khan <93315183+KSAIM09@users.noreply.github.com>
2024-09-01 13:43:55 +0000

    Add

--- server/app.js
+++ server/app.js
@@ -7,11 +7,7 @@ const app = express();
 const Connection = require('./database/db')
 const Router = require('./routes/route')
 
-app.use(cors({
-    origin: 'https://todo-list-app-beryl-rho.vercel.app', // Allow only your Vercel domain
-    methods: 'GET,POST,PUT,DELETE', // Specify allowed methods
-    credentials: true // If you need to send cookies or authorization headers
-}));
+app.use(cors());
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }))
 
@@ -20,5 +16,5 @@ app.use('/', Router);
 Connection();
 // server
 app.listen(process.env.PORT || 8000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 8000');
 })
