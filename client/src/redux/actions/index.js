aee6382b5343556258918ee11ffe7e42e0dc1e46
Saim Khan <93315183+KSAIM09@users.noreply.github.com>
2024-09-01 13:46:26 +0000

    api

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import { ADDNEW_TODO, GETALL_TODO, TOGGLE_TODO, UPDATE_TODO, DELETE_TODO, TOGGLE_TAB } from './type';
 
-const API_URL = 'https://todo-list-app-qp5p.onrender.com';
+const API_URL = 'https://todo-list-app-bfji.onrender.com';
 
 export const addNewTodo = (data) => async (dispatch) => {
     try {
